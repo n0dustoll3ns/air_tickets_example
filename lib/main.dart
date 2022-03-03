a526e8e064b9efb38988d034a15fb33478621121
n0dustoll3ns <fominykh.dg@gmail.com>
2022-03-03 20:15:09 +0700

    Change word "Hello Wolrd", to random english words

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,8 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
