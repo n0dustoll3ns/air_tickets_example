a11c6d8875abe946e501401a8996ad7818b16aff
n0dustoll3ns <fominykh.dg@gmail.com>
2022-10-02 18:08:49 +0300

    replace text buttons with dropdown buttons in filters

--- lib/global/global_const.dart
+++ lib/global/global_const.dart
@@ -19,6 +19,23 @@ const List<String> gTo = [
   'Palermo',
 ];
 
+const List<String> passengersNumber = [
+  '1 adult',
+  '2 adults',
+  '2 adults, \r1 child',
+];
+
+const List<String> wayEnd = [
+  '1 way trip',
+  'Round trip',
+];
+
+const List<String> ticketClass = [
+  'Business class',
+  '1st class',
+  'Economy class',
+];
+
 const EdgeInsets cellPadding = EdgeInsets.symmetric(vertical: 22);
 const EdgeInsets pagePadding = EdgeInsets.symmetric(horizontal: 22);
 const TextStyle cellTextStyle = TextStyle(color: Colors.white, fontSize: 16);
--- lib/screens/main_page.dart/components/search_filters.dart
+++ lib/screens/main_page.dart/components/search_filters.dart
@@ -2,8 +2,22 @@ import 'package:flutter/material.dart';
 
 import '../../../global/global_const.dart';
 
-class SearchFilters extends StatelessWidget {
+class SearchFilters extends StatefulWidget {
   const SearchFilters({super.key});
+
+  @override
+  State<SearchFilters> createState() => _SearchFiltersState();
+}
+
+class _SearchFiltersState extends State<SearchFilters> {
+  String wayEndValue = wayEnd.first;
+  String passengersValue = passengersNumber.first;
+  String ticketClassValue = ticketClass.last;
+  
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,22 +29,77 @@ class SearchFilters extends StatelessWidget {
         child: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            TextButton(style: btnStyle, onPressed: () {}, child: const Text('Round trip ⯆')),
+            DropdownButton<String>(
+              dropdownColor: Theme.of(context).backgroundColor,
+              value: wayEndValue,
+              icon: const Icon(Icons.arrow_drop_down),
+              elevation: 16,
+              style: const TextStyle(color: Colors.black),
+              underline: Container(
+                height: 0,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String? value) {
+                setState(() {
+                  wayEndValue = value!;
+                });
+              },
+              items: wayEnd.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
             const SizedBox(
               width: 26,
             ),
-            TextButton(
-              style: btnStyle,
-              onPressed: () {},
-              child: const Text('1 Adult ⯆'),
+            DropdownButton<String>(
+              dropdownColor: Theme.of(context).backgroundColor,
+              value: passengersValue,
+              icon: const Icon(Icons.arrow_drop_down),
+              elevation: 16,
+              style: const TextStyle(color: Colors.black),
+              // underline: Container(
+              //   height: 0,
+              //   color: Colors.deepPurpleAccent,
+              // ),
+              onChanged: (String? value) {
+                setState(() {
+                  passengersValue = value!;
+                });
+              },
+              items: passengersNumber.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
             const SizedBox(
               width: 26,
             ),
-            TextButton(
-              style: btnStyle,
-              onPressed: () {},
-              child: const Text('Economy ⯆'),
+            DropdownButton<String>(
+              dropdownColor: Theme.of(context).backgroundColor,
+              value: ticketClassValue,
+              icon: const Icon(Icons.arrow_drop_down),
+              elevation: 16,
+              style: const TextStyle(color: Colors.black),
+              // underline: Container(
+              //   height: 0,
+              //   color: Colors.deepPurpleAccent,
+              // ),
+              onChanged: (String? value) {
+                setState(() {
+                  ticketClassValue = value!;
+                });
+              },
+              items: ticketClass.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
           ],
         ),
